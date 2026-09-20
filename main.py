b60b231c6d71e8f23154335b2022ea2912c878ee
Nishant Sharma <nishantsharma5330@gmail.com>
2026-09-20 18:15:47 +0530

    Refactor to include document loading and processing

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from dotenv import load_dotenv
 # use groq ai because it is free
 from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-model = ChatGroq(
-     model="openai/gpt-oss-120b"
-)
+data = PyPDFLoader("document loaders/deeplearning.pdf")
+docs = data.load()
 
-result = model.invoke("Hello")
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200,
+)
+chunks = splitter.split_documents(docs)
+
+template = ChatPromptTemplate.from_messages(
+    [("system","you are AI that summerizes the text"),
+     ("human","{data}")
+     ]
+)
+model = ChatGroq(model="openai/gpt-oss-120b")
+
+prompt = template.format_messages(data = docs[0].page_content)
+
+result = model.invoke(prompt)
 
 print(result.content)
